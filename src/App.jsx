4f73fc07297b022a8bf35f97286a38c9a7e9dd74
Charity Sandra Hombarume <charityshombarume03@gmail.com>
2026-05-11 09:58:24 -0800

    adding data that is buildings, pathways and routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,3 +3,36 @@ import Map from "./components/Map";
 export default function App() {
   return <Map />;
 }
+
+import Buildings from "./components/Buildings";
+
+export function App() {
+  return (
+    <div>
+      <Buildings />
+    </div>
+  );
+}
+
+
+
+import Routes from "./components/Routes";
+
+function App() {
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+}
+
+
+import Pathways from "./components/Pathways";
+
+function App() {
+  return (
+    <div>
+      <Pathways />
+    </div>
+  );
+}
